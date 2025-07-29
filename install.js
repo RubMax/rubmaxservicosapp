e159d8a4b49cc19e23215615fdad12517ddf07f9
RubMax <54785672+RubMax@users.noreply.github.com>
2025-07-29 16:34:55 -0300

    Update install.js

--- install.js
+++ install.js
@@ -36,3 +36,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(reg => console.log("✅ SW enregistré", reg))
+      .catch(err => console.warn("❌ Erreur SW", err));
+  }
